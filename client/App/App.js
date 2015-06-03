b80fe7c4dda94c910a6554bb92f8c12935cfec8d
Ian Sinnott <ian@iansinnott.com>
2015-06-02 21:37:14 -0700

    Rename Layout to App

---
+++ client/App/App.js
@@ -0,0 +1,19 @@
+var React  = require('react');
+
+require('./Layout.styl');
+
+require('font-awesome-webpack');
+
+var App = React.createClass({
+  render() {
+    return (
+      <div className="app">
+        <h1>React Boilerplate</h1>
+        <p>What you know about React?</p>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
+
