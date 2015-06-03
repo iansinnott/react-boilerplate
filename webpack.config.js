d6d4d0f5b333e61544d7684808a0c1f9c50343a3
Ian Sinnott <ian@iansinnott.com>
2015-06-02 21:03:24 -0700

    Add production webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,60 @@
+/* eslint-disable camelcase */
+
 /**
- * Webpack configuration.
+ * Webpack production configuration.
  *
  * For more info check out the [docs][1] as well as [this article][2].
  *
  * [1]: http://webpack.github.io/docs/
  * [2]: http://thetrendythings.com/read/20178
+ *
+ * TODO: There is a lot in here that's repeated in ./webpack.config.dev.js so it
+ * would be a good idea to DRY out these two files
  */
 
-var webpack = require('webpack'),
+var webpack = require('webpack');
 
-    // Stylus
-    axis        = require('axis'),
-    rupture     = require('rupture'),
-    typographic = require('typographic');
+// Stylus
+var axis        = require('axis');
+var rupture     = require('rupture');
+var typographic = require('typographic');
 
-var WEBPACK_URL = 'http://localhost:8080/'; // Trailing slash is important
+var WEBPACK_URL = '/';
 
 module.exports = {
 
-  devtool: 'eval',
-
-  entry: [
-    'webpack-dev-server/client?' + WEBPACK_URL,
-    'webpack/hot/only-dev-server',
-    './client/index'
-  ],
+  entry: {
+    app: './client/index.js'
+  },
 
   output: {
     path: __dirname + '/public/',
-    filename: 'bundle.js',
-
-    // I'm not sure if this will cause problems when building for productiong.
-    // We don't want our build to be tied to any specific domain or URL. Not
-    // sure if this will affect that.
+    filename: '[name].js',
     publicPath: WEBPACK_URL
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(), // Enable HMR
-    new webpack.NoErrorsPlugin() // Don't reload on errors
+
+    // Don't load locales when requiring moment.js. We only need english
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+
+    // Minify
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      comments: /.^/, // This matches nothing and thus removes all comments
+      screw_ie8: true,
+
+      // Options: http://lisperator.net/uglifyjs/compress
+      compress: {
+        warnings: false
+      }
+    }),
+
+    // Dedupe
+    new webpack.optimize.DedupePlugin(),
+
+    // Don't reload on errors
+    new webpack.NoErrorsPlugin()
   ],
 
   // If you want to use files with the actual JSX extension add it to the list
@@ -46,12 +62,15 @@ module.exports = {
     extensions: ['', '.js']
   },
 
+  /**
+   * Note: css-lader is passed the no-restructuring param b/c it can sometimes
+   * cause issues where selectors will be reordered and no longer get applied.
+   */
   module: {
     loaders: [
       { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
-      { test: /\.styl$/, loaders: ['style', 'css', 'autoprefixer', 'stylus'] },
+      { test: /\.styl$/, loaders: ['style', 'css?-restructuring', 'autoprefixer', 'stylus'] },
       { test: /\.(png|jpg|gif)$/, loaders: ['file?name=[name].[ext]'] },
-      { test: /\.md$/, loaders: ['html', 'markdown'] },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
