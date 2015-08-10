2b1928d6344555ddded15a246bb4baa2f83eadc5
Ian Sinnott <ian@iansinnott.com>
2015-08-10 00:28:05 -0700

    Upgrade webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable camelcase */
+/* eslint-disable camelcase, no-var */
 
 /**
  * Webpack production configuration.
@@ -12,11 +12,12 @@
  * would be a good idea to DRY out these two files
  */
 
+var path = require('path');
 var webpack = require('webpack');
-
-// Stylus
-var axis        = require('axis');
-var rupture     = require('rupture');
+var strip = require('strip-loader');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var axis = require('axis');
+var rupture = require('rupture');
 var typographic = require('typographic');
 
 var WEBPACK_URL = '/';
@@ -24,42 +25,13 @@ var WEBPACK_URL = '/';
 module.exports = {
 
   entry: {
-    app: './client/index.js'
+    app: './client/index.js',
   },
 
   output: {
-    path: __dirname + '/public/',
+    path: path.resolve(__dirname, 'public/'),
     filename: '[name].js',
-    publicPath: WEBPACK_URL
-  },
-
-  plugins: [
-
-    // Don't load locales when requiring moment.js. We only need english
-    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
-
-    // Minify
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
-      comments: /.^/, // This matches nothing and thus removes all comments
-      screw_ie8: true,
-
-      // Options: http://lisperator.net/uglifyjs/compress
-      compress: {
-        warnings: false
-      }
-    }),
-
-    // Dedupe
-    new webpack.optimize.DedupePlugin(),
-
-    // Don't reload on errors
-    new webpack.NoErrorsPlugin()
-  ],
-
-  // If you want to use files with the actual JSX extension add it to the list
-  resolve: {
-    extensions: ['', '.js']
+    publicPath: WEBPACK_URL,
   },
 
   /**
@@ -68,16 +40,56 @@ module.exports = {
    */
   module: {
     loaders: [
-      { test: /\.js?$/, loaders: ['babel'], exclude: /node_modules/ },
-      { test: /\.styl$/, loaders: ['style', 'css?-restructuring', 'autoprefixer', 'stylus'] },
+      // See .babelrc for Babel config. Also note that all calls to console.log
+      // and debug will be stripped from the code
+      { test: /\.js?$/, loaders: [strip.loader('debug', 'console.log'), 'babel'], exclude: /node_modules/ },
+
+      { test: /\.styl$/, loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=2!autoprefixer!stylus') },
       { test: /\.(png|jpg|gif)$/, loaders: ['file?name=[name].[ext]'] },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['url?limit=10000&minetype=application/font-woff'] },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['file'] }
-    ]
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['file'] },
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css', { allChunks: true }),
+
+    // Don't load locales when requiring moment.js. We only need english
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+
+    // Minify
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+
+      // This matches nothing and thus removes all comments
+      comments: /.^/,
+      screw_ie8: true,
+
+      // Options: http://lisperator.net/uglifyjs/compress
+      compress: { warnings: false },
+    }),
+
+    // Dedupe
+    new webpack.optimize.DedupePlugin(),
+
+    // Don't reload on errors
+    new webpack.NoErrorsPlugin(),
+
+    // Define magic globals
+    new webpack.DefinePlugin({
+      __CLIENT__: true,
+      __SERVER__: false,
+      __DEV__: false,
+    }),
+  ],
+
+  // If you want to use files with the actual JSX extension add it to the list
+  resolve: {
+    extensions: ['', '.js'],
   },
 
   stylus: {
-    use: [axis(), rupture(), typographic()]
-  }
+    use: [axis(), rupture(), typographic()],
+  },
 
 };
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-var */
 /**
  * Webpack configuration.
  *
@@ -7,11 +8,10 @@
  * [2]: http://thetrendythings.com/read/20178
  */
 
+var path = require('path');
 var webpack = require('webpack');
-
-// Stylus
-var axis        = require('axis');
-var rupture     = require('rupture');
+var axis = require('axis');
+var rupture = require('rupture');
 var typographic = require('typographic');
 
 var DEV_PORT = process.env.DEV_PORT || 8888;
@@ -19,46 +19,55 @@ var WEBPACK_URL = 'http://localhost:' + DEV_PORT + '/'; // Trailing slash is imp
 
 module.exports = {
 
-  devtool: 'eval',
+  devtool: 'inline-source-map',
 
   entry: {
     app: [
       'webpack-dev-server/client?' + WEBPACK_URL,
       'webpack/hot/only-dev-server',
-      './client/index.js'
-    ]
+      './client/index.js',
+    ],
   },
 
   output: {
-    path: __dirname + '/public/',
+    path: path.resolve(__dirname, 'public/'),
     filename: '[name].js',
-    publicPath: WEBPACK_URL
+    publicPath: WEBPACK_URL,
   },
 
   // We't load locales when requiring moment.js. We usually only need english
   plugins: [
     new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]), // No locales
     new webpack.HotModuleReplacementPlugin(),              // Enable HMR
-    new webpack.NoErrorsPlugin()                           // Don't reload on errors
+    new webpack.NoErrorsPlugin(),                          // Don't reload on errors
+    new webpack.DefinePlugin({                             // Define magic globals
+      __CLIENT__: true,
+      __SERVER__: false,
+      __DEV__: true,
+    }),
   ],
+
+  // Show progress
+  progress: true,
 
   // If you want to use files with the actual JSX extension add it to the list
   resolve: {
-    extensions: ['', '.js']
+    extensions: ['', '.js'],
   },
 
   module: {
     loaders: [
+      // See .babelrc for Babel config
       { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
-      { test: /\.styl$/, loaders: ['style?singleton', 'css', 'autoprefixer', 'stylus'] },
+      { test: /\.styl$/, loaders: ['style', 'css?modules&importLoaders=2&localIdentName=[name]__[local]___[hash:base64:6]', 'autoprefixer', 'stylus'] },
       { test: /\.(png|jpg|gif)$/, loaders: ['file?name=[name].[ext]'] },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['url?limit=10000&minetype=application/font-woff'] },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['file'] }
-    ]
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['file'] },
+    ],
   },
 
   stylus: {
-    use: [axis(), rupture(), typographic()]
-  }
+    use: [axis(), rupture(), typographic()],
+  },
 
 };
