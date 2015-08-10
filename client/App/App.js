300d0a86b0e745fed757b3473c63e8a26aa94a6b
Ian Sinnott <ian@iansinnott.com>
2015-08-10 00:31:15 -0700

    Use localized styles

--- client/App/App.js
+++ client/App/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import './App.styl';
+import styles from './App.styl';
 
 import 'font-awesome-webpack';
 
 export default React.createClass({
   render() {
     return (
-      <div className="App container">
+      <div className={styles.App}>
         <h1>React Boilerplate</h1>
         <p>What you know about React?</p>
       </div>
