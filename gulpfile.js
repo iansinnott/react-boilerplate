8f0e70cafc731fb8fa95fbb7bfeb5ac5105e51a2
Ian Sinnott <ian@iansinnott.com>
2015-08-16 14:16:01 -0700

    Clean up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,15 @@
 /* eslint-disable no-var, vars-on-top */
-
-// General
+require('colors');
 var gulp = require('gulp');
 var fs = require('fs');
-var url = require('url');
-
-// String coloring
-require('colors');
-
-// Used with mongo
-var exec = require('child_process').exec;
 var rmdir = require('rimraf');
-
-// Nodemon
 var nodemon = require('nodemon');
-
-// Webpack
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 
 // Config
 var config = require('./webpack.config');
-var devConfig = require('./webpack.dev.config');
-var publicPath = devConfig.output.publicPath;
+var publicPath = config.output.publicPath;
 
 // TODO: This is just the waterline-disk default. Move this into config so that
 // it can be modified in one place.
@@ -31,7 +18,7 @@ var DB_PATH = './tmp';
 /**
  * Build app for production
  */
-gulp.task('build', ['clean'], function(done) {
+gulp.task('build', function(done) {
   console.log('Webpack bundle', 'compiling...');
   webpack(config, function(err, stats) {
     if (err)
@@ -68,43 +55,6 @@ gulp.task('nodemon', function() {
   });
 });
 
-/**
- * Remove bundled files from public/
- * Note: This does not remove the public directory itself
- */
-gulp.task('clean', function(done) {
-  try {
-    fs.readdirSync('public').forEach(function(filename) {
-      fs.unlinkSync('public/' + filename);
-    });
-    console.log('public/'.green, 'directory cleaned.');
-  } catch (e) {
-    console.log('Error:'.red, 'Could not clean public directory');
-  }
-  done();
-});
-
-gulp.task('mongod', function() {
-  // Make sure the db directory exists. If not then create it.
-  try {
-    fs.readdirSync(DB_PATH);
-  } catch (e) {
-    fs.mkdir(DB_PATH);
-  }
-
-  // Run mongod
-  exec('mongod --dbpath ./.db', function(err) {
-    if (err) throw err;
-    console.log('MongoDB started');
-  });
-
-  console.log(
-    'MongoDB server'.green,
-    'listening on port',
-    '27017'.magenta
-  );
-});
-
 gulp.task('reset', function() {
   try {
     rmdir.sync(DB_PATH);
@@ -118,39 +68,13 @@ gulp.task('reset', function() {
   }
 });
 
-gulp.task('postgres', function() {
-  console.log('WARNING:'.yellow, 'Not yet implemented. Postgres support is coming but not here yet.');
-
-  // Run postgres
-  // exec('postgres -D ' + DB_PATH, function(err) {
-  //   if (err) throw err;
-  //   console.log('Postgres started');
-  // });
-
-  // console.log(
-  //   'Postgres server'.green,
-  //   'listening at',
-  //   'localhost:5432'.magenta
-  // );
-});
-
 /**
  * Run the webpack dev server. This should always be run in tandem with the
  * nodemon dev server above.
  */
 gulp.task('webpack', function() {
-  var DEV_PORT = url.parse(publicPath).port;
-
-  var server = new WebpackDevServer(webpack(devConfig), {
-    contentBase: './public/',
-    publicPath: publicPath,
-    hot: true,
-    inline: true,
-    stats: {
-      colors: true,
-      chunks: false,
-    },
-  });
+  var DEV_PORT = config.devServer.port;
+  var server = new WebpackDevServer(webpack(config), config.devServer);
 
   server.listen(DEV_PORT, function(err, result) {
     if (err) console.error(err);
