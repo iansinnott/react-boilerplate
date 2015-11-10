c0f1b49183b716c3ee9ec58b52cb214c34713c02
Ian Sinnott <ian@iansinnott.com>
2015-11-10 13:02:14 -0800

    Update package.json, remove boosh.js dependency

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-var */
-var path = require('path');
-var boosh = require('booshjs');
-module.exports = boosh({
-  in: { app: './client/index.js' },
-  out: path.resolve(__dirname, 'public/'),
-  isDev: process.env.NODE_ENV === 'development',
-});
