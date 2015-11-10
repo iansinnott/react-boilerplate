2e82d74df78d5b2daceb45054b6a3c20e171ecf3
Ian Sinnott <ian@iansinnott.com>
2015-11-10 13:02:25 -0800

    Add webpack dev and prod configurations

---
+++ webpack.config.dev.js
@@ -0,0 +1,65 @@
+/* eslint-disable */
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+  devtool: 'inline-source-map',
+
+  entry: {
+    app: [
+      'webpack-hot-middleware/client',
+      './src/index'
+    ]
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/static/'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      },
+      {
+        test: /\.sass$/,
+        loaders: [
+          'style',
+          'css?modules&importLoaders=2&localIdentName=[name]__[local]__[hash:base64:6]',
+          'postcss',
+          'sass?indentedSyntax'
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loaders: ['url?limit=10000&mimetype=application/font-woff']
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loaders: ['file']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loaders: ['file?name=[name].[ext]']
+      },
+    ]
+  },
+
+  postcss: [
+    autoprefixer({ browsers: ['last 2 versions', 'ie >= 10'] })
+  ]
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,65 @@
+/* eslint-disable */
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    app: ['./src/index']
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css', { allChunks: true }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css'),
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=2!postcss!sass?indentedSyntax')
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loaders: ['url?limit=10000&mimetype=application/font-woff']
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loaders: ['file']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loaders: ['file?name=[name].[ext]']
+      },
+    ]
+  },
+
+  postcss: [
+    autoprefixer({ browsers: ['last 2 versions', 'ie >= 10'] })
+  ],
+
+};
