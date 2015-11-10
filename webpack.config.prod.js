5ca0e58ad3d1c106df3dabbde87e748d24fde01b
Ian Sinnott <ian@iansinnott.com>
2015-11-10 14:08:34 -0800

    Use 'public' instead of 'dist' for compiled files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,7 +10,7 @@ module.exports = {
     app: ['./src/index']
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public'),
     filename: '[name].js',
     publicPath: '/static/'
   },
